e9337c3c8f3ce19fda73713c5548970136af1c91
leitolima <leo-lima-08@hotmail.com>
2021-02-27 18:19:31 -0300

    svg icons

--- components/sidebar/NavItem.js
+++ components/sidebar/NavItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const NavItem = ({ icon, text }) => {
+const NavItem = ({ children, text }) => {
     return (
         <li className="text-xl py-3">
             <p className="text-gray-200 flex flex-row items-center">
-                <svg className="w-12 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={icon}/></svg>
+                {children}
                 {text}
             </p>
         </li>
--- components/sidebar/Navigation.js
+++ components/sidebar/Navigation.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import NavItem from './NavItem';
+//Icons
+import Home from '../../public/home.svg';
+import User from '../../public/user.svg';
+import Puzzle from '../../public/puzzle.svg';
+import CV from '../../public/cv.svg';
+import Mail from '../../public/mail.svg';
 
 const Navigation = () => {
     return (
         <nav className="flex-1 w-full m-auto mt-6 px-4">
             <ul className="list-none">
-            {/* <svg d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
-            <svg d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" /></svg>
-            <svg d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" /></svg>
-            <svg d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg> */}
-                <NavItem text="Inicio" icon="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
-                <NavItem text="Sobre mí" icon="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
-                <NavItem text="Habilidades" icon="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
-                <NavItem text="Currículum" icon="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"/>
-                <NavItem text="Contacto" icon="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
+                <NavItem text="Inicio"><Home/></NavItem>
+                <NavItem text="Sobre mí"><User/></NavItem>
+                <NavItem text="Habilidades"><Puzzle/></NavItem>
+                <NavItem text="Currículum"><CV/></NavItem>
+                <NavItem text="Contacto"><Mail/></NavItem>
             </ul>
         </nav>
     )
